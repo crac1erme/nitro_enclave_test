b5eed07a42d2924299d7ac33602b8b418079e3c8
josh <josh@abc>
2025-12-29 19:15:22 +0800

    fix

---
+++ socat-proxy.sh
@@ -0,0 +1,2 @@
+ENCLAVE_CID=$(nitro-cli describe-enclaves | jq -r ".[0].EnclaveCID")
+docker run -d --name enclave-vsock-proxy --network host --privileged alpine/socat TCP-LISTEN:8080,fork,reuseaddr VSOCK-CONNECT:${ENCLAVE_CID}:8001
