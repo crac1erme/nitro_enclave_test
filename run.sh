5ca9638fe77f685a30685fefda552ab4dce2b450
josh <josh@abc>
2025-12-29 18:50:27 +0800

    fix

--- run.sh
+++ run.sh
@@ -3,5 +3,9 @@ ip addr add 127.0.0.1/32 dev lo
 
 ip link set dev lo up
 
+# Add a hosts record, pointing target site calls to local loopback
+echo "127.0.0.1   dynamodb.us-west-2.amazonaws.com" >> /etc/hosts
+
 /enclave-server &
 socat VSOCK-LISTEN:8001,fork,reuseaddr TCP:127.0.0.1:8080 &
+socat TCP-LISTEN:443,fork,reuseaddr VSOCK-CONNECT:3:8002
